02a3932034b74e29c2e6456f321e12da50b62811
Neordan <83787849+Neordan@users.noreply.github.com>
2023-05-23 23:35:13 +0200

    Affichage des instructions des plats

--- src/components/Card.js
+++ src/components/Card.js
@@ -10,13 +10,15 @@ const Card = ( { dish }) => {
             {/* Affiche l'origine du plat en utilisant la propriété "strArea" de l'objet "dish"
             La valeur de "strArea" représente la région d'origine du plat */}
 
-            <p>Origin : {dish.strArea}</p>
+            <p className="origin">Origin : {dish.strArea}</p>
 
             {/* Affiche une image du plat en utilisant la propriété "strMealThumb" de l'objet "dish"
             La valeur de "strMealThumb" est l'URL de l'image du plat
             L'attribut "alt" de l'image est défini comme "photo de [nom du plat]" pour une alternative textuelle */}
 
             <img src={dish.strMealThumb} alt={"photo de " + dish.strMeal} />
+
+            <p className="instruction">{dish.strInstructions}</p>
         </div>
     );
 };
