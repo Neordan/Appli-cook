9dd6c6762020c80f7c111e6ea4c9f0185093d098
Neordan <83787849+Neordan@users.noreply.github.com>
2023-05-23 21:13:15 +0200

    Update App.js
    html && création useEffect pour la requête avec l'API

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 
 const App = () => {
+
+  const [dishData, setDishData] = useState([]);
+
+   // Effectue une requête GET vers l'API MealDB pour rechercher des plats contenant "tomato" dans leur nom et affiche les
+ 
+  useEffect(() => {
+    axios
+    .get("https://www.themealdb.com/api/json/v1/1/search.php?s=tomato")
+    .then((res) => console.log(res.data.meals))
+  })
+
   return (
-    <div>
-      
+    <div className="app-container">
+      <h1>Cooking Application with React</h1>
+      {/* input pour la recherche  */}
+      <input type="text" placeholder="Search by ingredient" />
+      {/* container pour les plats */}
+      <div className="dish-container">
+
+      </div>
     </div>
   );
 };
