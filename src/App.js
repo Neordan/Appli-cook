4f4ff1912569fba0287927919375ea8681e15825
Neordan <83787849+Neordan@users.noreply.github.com>
2023-05-24 00:33:55 +0200

    Ajout de la fonction slice pour afficher jusqu'à 24 cards

--- src/App.js
+++ src/App.js
@@ -12,28 +12,29 @@ const App = () => {
       .get("https://www.themealdb.com/api/json/v1/1/search.php?s=" + inputSearch)
       .then((res) => setDishesData(res.data.meals)) // Met à jour les données des plats avec les résultats de la requête
       .catch((error) => console.error(error)); // Gère les erreurs en les affichant sur la console
-  }, []);
+  }, [inputSearch]);
 
   return (
     <div className="app-container">
       <h1>Cooking Application with React</h1>
       {/* input pour la recherche */}
-      <input 
-      type="text" 
-      placeholder="Search by ingredient"
-      onChange={(e) => setInputSearch(e.target.value)} 
+      <input
+        type="text"
+        placeholder="Search by ingredient"
+        onChange={(e) => setInputSearch(e.target.value)}
       />
       {/* container pour les plats */}
       <div className="dish-container">
         {/* Boucle sur les données des plats pour afficher chaque plat */}
         {dishesData
-        
-        .map((dish) => (
-          // Rendu d'un composant "Card" pour afficher les détails d'un plat
-          // La clé unique "key" est définie sur "dish.idMeal" pour optimiser le rendu des éléments
-          // Les données du plat sont passées au composant "Card" via la prop "dish"
-          <Card key={dish.idMeal} dish={dish} />
-        ))}
+          // Afficher les cards de 0 à 24 
+          .slice(0, 24)
+          .map((dish) => (
+            // Rendu d'un composant "Card" pour afficher les détails d'un plat
+            // La clé unique "key" est définie sur "dish.idMeal" pour optimiser le rendu des éléments
+            // Les données du plat sont passées au composant "Card" via la prop "dish"
+            <Card key={dish.idMeal} dish={dish} />
+          ))}
       </div>
     </div>
   );
