31ffaa38326dac1de097a4b6e58226868564dcff
Neordan <83787849+Neordan@users.noreply.github.com>
2023-05-23 22:07:57 +0200

    Essaie pour afficher les infos de l'API dans un h2

--- src/App.js
+++ src/App.js
@@ -2,25 +2,26 @@ import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 
 const App = () => {
+  const [dishesData, setDishesData] = useState([]);
 
-  const [dishData, setDishData] = useState([]);
-
-   // Effectue une requête GET vers l'API MealDB pour rechercher des plats contenant "tomato" dans leur nom et affiche les
- 
+  // Effectue une requête GET vers l'API MealDB pour rechercher des plats contenant "tomato" dans leur nom et met à jour les données des plats
   useEffect(() => {
     axios
-    .get("https://www.themealdb.com/api/json/v1/1/search.php?s=tomato")
-    .then((res) => console.log(res.data.meals))
-  })
+      .get("https://www.themealdb.com/api/json/v1/1/search.php?s=tomato")
+      .then((res) => setDishesData(res.data.meals)) // Met à jour les données des plats avec les résultats de la requête
+      .catch((error) => console.error(error)); // Gère les erreurs en les affichant sur la console
+  }, []);
 
   return (
     <div className="app-container">
       <h1>Cooking Application with React</h1>
-      {/* input pour la recherche  */}
+      {/* input pour la recherche */}
       <input type="text" placeholder="Search by ingredient" />
       {/* container pour les plats */}
       <div className="dish-container">
-
+        {dishesData.map((dish) => (
+          <h2 key={dish.idMeal}>{dish.strMeal}</h2> // Affiche les noms des plats en utilisant les données récupérées
+        ))}
       </div>
     </div>
   );
