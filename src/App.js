5dff5df17ca4b5ac9255ed3325f21059cfebd57c
Neordan <83787849+Neordan@users.noreply.github.com>
2023-05-24 03:14:43 +0200

    Utilisation d'un useRef pour la recherche

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 import axios from 'axios';
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import Card from './components/Card';
 
 const App = () => {
   const [dishesData, setDishesData] = useState([]);
-  const [inputSearch, setInputSearch] = useState("");
+  const searchInputRef = useRef(null);
 
-  // Effectue une requête GET vers l'API MealDB pour rechercher des plats contenant "tomato" dans leur nom et met à jour les données des plats
-  useEffect(() => {
+  // Effectue une requête GET vers l'API MealDB pour rechercher des plats contenant le terme de recherche dans leur nom et met à jour les données des plats
+  const handleSearch = () => {
+    const searchTerm = searchInputRef.current.value;
     axios
-      .get("https://www.themealdb.com/api/json/v1/1/search.php?s=" + inputSearch)
+      .get("https://www.themealdb.com/api/json/v1/1/search.php?s=" + searchTerm)
       .then((res) => setDishesData(res.data.meals)) // Met à jour les données des plats avec les résultats de la requête
       .catch((error) => console.error(error)); // Gère les erreurs en les affichant sur la console
-  }, [inputSearch]);
+  };
 
   return (
     <div className="app-container">
@@ -21,15 +22,16 @@ const App = () => {
       <input
         type="text"
         placeholder="Search by ingredient"
-        onChange={(e) => setInputSearch(e.target.value)}
+        ref={searchInputRef}
       />
+      {/* Bouton pour déclencher la recherche */}
+      <button onClick={handleSearch}>Search</button>
       {/* container pour les plats */}
       <div className="dish-container">
         {/* Boucle sur les données des plats pour afficher chaque plat */}
         {dishesData &&
           // Afficher les cards de 0 à 24 
-          dishesData.slice(0, 24)
-          .map((dish) => (
+          dishesData.slice(0, 24).map((dish) => (
             // Rendu d'un composant "Card" pour afficher les détails d'un plat
             // La clé unique "key" est définie sur "dish.idMeal" pour optimiser le rendu des éléments
             // Les données du plat sont passées au composant "Card" via la prop "dish"
